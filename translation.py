5fa276284f705bdbc47a78ae9b03f598a601c06a
MAWHBots <160210473+MAWHBots@users.noreply.github.com>
2024-08-13 20:27:51 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -54,10 +54,10 @@ class Translation(object):
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a>
-<b>🧑‍💻 Developer :</b> <a href='https://t.me/CallAdminRobot'>Jishu Developer</a>
+<b>📢 Channel :</b> <a href='https://t.me/moviesandwebserieshubOrzz'> Botz</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/OrzzQueryBot'>Jishu Developer</a>
 
-<b>♻️ Bot Made By :</b> @Madflix_Bots"""
+<b>♻️ Bot Made By :</b> @moviesandwebserieshubOrzz"""
   
   STATUS_TXT = """<b><u>Bot Status</u></b>
   
